c5c7ed754ae7c7bd3a6006266e58ea22f3d64b5e
Adam Julier <adamjulier@Kevins-Air.home>
2018-03-25 21:37:48 -0400

    updates

--- projects/tic-tac-toe-game/script.js
+++ projects/tic-tac-toe-game/script.js
@@ -18,7 +18,7 @@ var currentTurn = "";
 function updateGameBoard(node) {
   //checks game isn't over, if it's won then does nothing
   if (gameWon === 1) {
-    console.log('Game completed, restart to play again');
+      console.log('Game completed, restart to play again');
   } else { //**** Runs main game logic
         //check that clicked node hasn't already been selected, or else stops counter
         if ($gameBoard[node] > 0) {
@@ -61,10 +61,11 @@ $('td').on('click', function() {
 
 
 
-function player(num, name, sign) {
+function player(num, name, sign, color) {
     this.playerNum = num;
     this.playerName = name;
     this.$playerSign = sign;
+    this.$playerColor = color;
 
     this.$playerBoard = [
     0, 0, 0,
@@ -74,15 +75,18 @@ function player(num, name, sign) {
 
     //update message showing the current turn
   player.prototype.takeTurn = function() {
-    let $message = $('h3.messages');
-    $message.text(`Current turn: Player ${this.playerNum}`);
+          let $message = $('h3.messages');
+          $message.text(`Current turn: Player ${this.playerNum}`);
+          $message.css('color', this.$playerColor);
+
 
   },
 
   player.prototype.addSign = function(node) {
     //console.log(`P${this.playerNum} selected: ${node}`);    //can comment out
     $('td').eq(node).append('<h1>').text(this.$playerSign);
-    $('td:after').eq(node).css('padding', '10%');
+    $('td').eq(node).css('color', this.$playerColor);
+
   },
 
   player.prototype.winCheck = function() {
@@ -92,6 +96,9 @@ function player(num, name, sign) {
             this.$playerBoard[3] + this.$playerBoard[4] + this.$playerBoard[5] === 3 ||
             this.$playerBoard[6] + this.$playerBoard[7] + this.$playerBoard[8] === 3) {
                 gameWon = 1;
+                let $message = $('h3.messages');
+                $message.text(`Tic Tac Toe! Player ${this.playerNum} wins.`);
+                $message.css('color', this.$playerColor);
                 alert(`Tic Tac Toe! Player ${this.playerNum} wins.`);
                 console.log(`Tic Tac Toe by row, Player ${this.playerNum} wins.`);
             }
@@ -101,6 +108,9 @@ function player(num, name, sign) {
          this.$playerBoard[1] + this.$playerBoard[4] + this.$playerBoard[7] === 3 ||
          this.$playerBoard[2] + this.$playerBoard[5] + this.$playerBoard[8] === 3) {
              gameWon = 1;
+             let $message = $('h3.messages');
+             $message.text(`Tic Tac Toe! Player ${this.playerNum} wins.`);
+             $message.css('color', this.$playerColor);
              alert(`Tic Tac Toe! Player ${this.playerNum} wins.`);
              console.log(`Tic tac toe by column, Player ${this.playerNum} wins.`);
          }
@@ -109,22 +119,22 @@ function player(num, name, sign) {
       if (this.$playerBoard[0] + this.$playerBoard[4] + this.$playerBoard[8] === 3 ||
           this.$playerBoard[2] + this.$playerBoard[4] + this.$playerBoard[6] === 3) {
               gameWon = 1;
+              let $message = $('h3.messages');
+              $message.text(`Tic Tac Toe! Player ${this.playerNum} wins.`);
+              $message.css('color', this.$playerColor);
               alert(`Tic Tac Toe! Player ${this.playerNum} wins.`);
               console.log(`Tic tac toe by diagonal, Player ${this.playerNum} wins.`);
 
       if (gameWon != 1) {
         return 0;
     }
-
     }
-
     }
-
   }
 
 
-var playerOne = new player(1, "One", "X");
-var playerTwo = new player(2, "Two", "O");
+var playerOne = new player(1, "One", "X", "red");
+var playerTwo = new player(2, "Two", "O", "blue");
 
 
 function turnUpdate() {
